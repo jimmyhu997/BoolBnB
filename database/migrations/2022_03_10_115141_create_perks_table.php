f744d8ded1da7ff90e95b18cb2390b907bbf9266
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2022-03-10 14:48:15 +0100

    Update - Perks Table

--- database/migrations/2022_03_10_115141_create_perks_table.php
+++ database/migrations/2022_03_10_115141_create_perks_table.php
@@ -17,8 +17,6 @@ class CreatePerksTable extends Migration
             $table->id();
             $table->string('name');
             $table->text('icon')->nullable();
-
-            $table->timestamps();
         });
     }
 
