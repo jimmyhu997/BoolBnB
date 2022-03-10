cbf758ea19c2826c4e3d0cef3ae75178b16335c8
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2022-03-10 14:50:49 +0100

    Update - Perk/SponsorPackages Models

--- app/Perk.php
+++ app/Perk.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Perk extends Model
 {
+    public $timestamps = false;
+
     public function stays() {
         return $this->belongsToMany('App\Stay');
     }
--- app/SponsorPackage.php
+++ app/SponsorPackage.php
@@ -5,7 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class SponsorPackage extends Model
-{
+{   
+    public $timestamps = false;
     public function stays() {
         return $this->belongsToMany('App\Stay')->withPivot('start_date', 'end_date')->withTimestamps();
     }
