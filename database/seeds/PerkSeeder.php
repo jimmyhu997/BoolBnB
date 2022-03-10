4ffc6d2910831eb6c0b31eebdb58f2d2d0985c60
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2022-03-10 13:16:11 +0100

    Create - Seeders

---
+++ database/seeds/PerkSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PerkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
