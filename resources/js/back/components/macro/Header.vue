eef79113219e13b2b28ec6523713505180928715
jimmyhu997 <jimmywx@outlook.it>
2022-03-11 18:13:36 +0100

    init back office dashboard header layout

--- resources/js/back/components/macro/Header.vue
+++ resources/js/back/components/macro/Header.vue
@@ -1,15 +1,30 @@
 <template>
   <header>
-      <h1>Ciaooo</h1>
+    <div class="icon">
+      <a href="/"><img src="" alt=""><h3>icona</h3></a>
+    </div>
+    <div class="middle_menu">
+      <ul>
+        <li><router-link :to="{name:'dashboard'}">Dashboard</router-link></li>
+        <li v-for="option,index in this.menu" :key="index" ><router-link :to="{name:'stays'}">My Apartment</router-link></li>
+      </ul>
+      
+    </div>
+    <div class="profile_options"> 
       <button @click="logout()">Logout</button>
-      <a href="/">HOME</a>
-      <router-link :to="{name:'dashboard'}">Dashboard</router-link>
+
+    </div>
   </header>
 </template>
 
 <script>
 export default {
     name: 'Header',
+    data(){
+      return {
+        menu: ['My Apartment']
+      }
+    },
     methods: {
       logout() {
         axios.post("/logout")
@@ -24,6 +39,34 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='scss' scoped>
+  header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: black;
+    height: 200px;
+    color: white;
+    .icon{
+      color: inherit;
+      a{
+        color:white;
+        text-decoration: none;
+      }
+    }
+    .middle_menu{
+      color: inherit;
+      ul{
+        display: flex;
+        list-style: none;
+        li{
+          margin: 20px;
+          a{
+            text-decoration: none;
+            color: inherit;
+          }
+        }
+      }
+    }
+  }
 </style>
