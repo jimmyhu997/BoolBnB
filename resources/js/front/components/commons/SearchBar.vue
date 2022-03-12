ad770af8bd845a242680458354731ca0c0b50885
Giovanni <gio.a.98@gmail.com>
2022-03-12 01:11:22 +0100

    header logo + menu

---
+++ resources/js/front/components/commons/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search">
+      <div class="search__wrapper">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
