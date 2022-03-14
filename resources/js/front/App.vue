6ec1ed6f0bb6a91c7b5472830a3137250454ec2f
Giovanni <gio.a.98@gmail.com>
2022-03-14 16:43:32 +0100

    update front App with common elements

--- resources/js/front/App.vue
+++ resources/js/front/App.vue
@@ -7,9 +7,9 @@
 </template>
 
 <script>
-import Header from './components/macro/Header.vue';
+import Header from '../vue-commons/macro/Header.vue';
 import Main from './components/macro/Main.vue';
-import Footer from './components/macro/Footer.vue';
+import Footer from '../vue-commons/macro/Footer.vue';
 import data from './global'
 
 export default {
@@ -33,3 +33,7 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+  
+</style>
