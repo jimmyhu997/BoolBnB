176f1d492f68fa098de5f67fd3f5fb1ab0c81124
Giorgio <1999giorgio.monne@gmail.com>
2022-03-15 01:04:55 +0100

    update back-office (checkout problem)

--- resources/js/back/components/pages/Dashboard.vue
+++ resources/js/back/components/pages/Dashboard.vue
@@ -21,7 +21,17 @@
                 </div>
 
                 <div class="user">
-                  <span>{{messaggio.user}} - {{messaggio.email}}</span>
+                  <img id="icon" src="//ssl.gstatic.com/ui/v1/icons/mail/profile_mask2.png" style="background-color: #cccccc">
+                  
+                  <div class="user-info">
+
+                    <div class="name">
+                      <span>{{messaggio.user}}</span>
+                      <span><span>➥ </span><a id="email" href="https://www.instagram.com/giorgiomonne/?hl=it">{{messaggio.email}}</a></span>
+                    </div>
+
+                    <span id="date">{{messaggio.date}}</span>
+                  </div>
                 </div>
 
                 <div class="content">
@@ -38,6 +48,7 @@
           </div>
         </div>
       </div>
+
 
       <!-- <div class="card">
         <span class="top">New messages</span>
@@ -71,8 +82,6 @@
 
 <script>
 
-import '../../../../sass/_variables.scss';
-
 export default {
     name: 'Dashboard',
     data() {
@@ -83,85 +92,99 @@ export default {
             title: 'Hotel room',
             user: 'pippofranco',
             email: 'pippo@gmail.com',
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."         
+            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+            date: "13:20"         
           },
           {
             title: 'Hostel room',
             user: 'pasticcio',
             email: 'pasticcio@gmail.com',
-            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic."          
+            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic." ,
+            date: "13:20"             
           }, 
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "Studenti costretti a ripetere per 8 anni la stessa classe Centinaia di studenti di una scuola remota della Papua Nuova ..."          
+            text: "Studenti costretti a ripetere per 8 anni la stessa classe Centinaia di studenti di una scuola remota della Papua Nuova ..." ,
+            date: "13:20"         
           },
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic."          
+            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic.",
+            date: "13:20"          
           },
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "pippone"          
+            text: "pippone" ,
+            date: "13:20"         
           }, 
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "pippone"          
+            text: "pippone",
+            date: "13:20"          
           },
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic."          
+            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic.",
+            date: "13:20"          
           },
           {
             title: 'Villa room',
             user: 'dio',
-            email: 'dio@gmail.com',
-            text: "pippone"          
+            email: '1999giorgio.monne@gmail.com',
+            text: "pippone",
+            date: "11:20"          
           }, 
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "pippone"          
+            text: "pippone",
+            date: "09:20"         
           },
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic."          
+            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic.",
+            date: "18:20"          
           },
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "pippone"          
+            text: "pippone",
+            date: "23:20"          
           }, 
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "pippone"          
+            text: "pippone",
+            date: "22:20"          
           },
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic."          
+            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptatem iusto. Perferendis vero nisi porro omnis reprehenderit quo facilis consectetur repudiandae ipsa quibusdam? Mollitia doloribus quos explicabo rem praesentium hic.",
+            date: "13:20"          
           },
           {
             title: 'Villa room',
             user: 'dio',
             email: 'dio@gmail.com',
-            text: "pippone"          
+            text: "pippone",
+            date: "13:20"          
           }
         ]
       }
@@ -177,82 +200,111 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .dashboard-content{
+@import '../../../../sass/_variables.scss';
+    .dashboard-content {
       width: 96%;
       margin: 2rem auto ;
 
-      h3{
+      h3 {
         padding: 2rem 0;
       }
 
-      .card{
+      .card {
         width: 100%;
-        border-radius: 0.4rem;
+        border-radius: .4rem;
         padding: 1rem;
         border: 1px solid rgb(115, 146, 169);
-        background-color: rgb(135, 173, 255);
+        background-color: $lightGrey;
 
-        .margine{
+        .margine {
           height: 500px;
         }
 
-        .search{
-          margin: 1rem 0;
+        // .search {
+        //   margin: 1rem 0;
 
-          input{
-            width: calc(100% - 100px);
-            padding: 5px;
-            border-radius: 0.5rem;
-          }
+        //   input {
+        //     width: calc(100% - 100px);
+        //     padding: 5px;
+        //     border-radius: .5rem;
+        //   }
 
-          button{
-            width:90px;
-            padding: 5px;
-            border-radius: 0.5rem;
-          }
-        }
+        //   button {
+        //     width:90px;
+        //     padding: 5px;
+        //     border-radius: .5rem;
+        //   }
+        // }
 
-        .top{
+        .top {
           font-size: 1.2rem;
           font-weight: bold;
         }
 
-        .messages{
+        .messages {
           margin: 1rem 0;
           height: 450px;
           overflow-y: auto;
 
           ul {
-            cursor: pointer;
+            
             li {
               width: 99%;
-              margin-bottom: 0.3rem;
-              margin-right: 0.2rem;
+              margin-bottom: .3rem;
+              margin-right: .2rem;
               border: 1px solid grey;
-              border-radius: 0.5rem;
+              border-radius: .5rem;
               list-style: none; 
               background-color: rgb(255, 255, 255);
-              
-              .title, .user{
-                width: 100%;
-                padding: 0.3rem 0.1rem;
-              }
 
-              .title{
+              .title {
+                width: 100%;
+                padding: .3rem .5rem;
                 font-weight: bold;
                 border-bottom: 1px solid grey;
               }
-
-              .content{
+              
+              .user {
                 width: 100%;
-                padding: 0.5rem 0.2rem;
+                padding: .3rem .2rem;
+                border-bottom: 1px solid lightgrey;
+                display: flex;
+                align-content: center; 
+
+                #icon {
+                  cursor: pointer;
+                  border-radius: 50%;
+                  margin: 0 .4rem  .2rem .2rem;
+                }
+
+                .user-info{
+                  width: 100%;
+                  display: flex;
+                  justify-content: space-between;
+                  align-items: center;
+
+                  .name{
+                    cursor: pointer;
+                  }
+                  
+                  #date{
+                    font-size: .8rem;
+                    margin-right: .6rem;
+                    color: rgb(182, 181, 181);
+                  }
+                }
+              }
+
+              .content {
+                width: 100%;
+                padding: .5rem .4rem;
                 word-wrap: break-word;               
               }
             }
           }
         }
 
-        .empty{
+        .empty {
           width: 100%;
           padding: 4rem;
           margin: 1rem 0;
@@ -262,7 +314,7 @@ export default {
           justify-content: center;
           align-items: center;
 
-          span{
+          span {
             font-weight: bold;
           }
         }
