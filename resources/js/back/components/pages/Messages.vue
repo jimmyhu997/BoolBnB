f56224bbc10c81b0bd9a1a34c31de45ea4f03ad9
Giorgio <1999giorgio.monne@gmail.com>
2022-03-16 11:32:38 +0100

    update array + structure inbox layout

--- resources/js/back/components/pages/Messages.vue
+++ resources/js/back/components/pages/Messages.vue
@@ -1,15 +1,76 @@
 <template>
-  <h2>messages</h2>
+  <div class="container">
+
+    <div class="menu">
+        dfgdf
+    </div>
+
+    <div class="messages">
+fghfgh
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Messages'
+    name: 'Messages',
+    data() {
+      return {
+        stays : [
+            {
+              name: 'Hotel room',
+              message: {
+                content: "sdf sdfg sdfg sdf sdf s drh sdfh  sdfh sdf sdf hgsdfh",
+                email: "ciccio@gmnail.com",
+                date: "12:20"
+              }
+            },
+            {
+              name: 'Hostel room',
+              message: {
+                content: "sdf sdfg sdfg sdf sdf s drh sdfh  sdfh sdf sdf hgsdfh",
+                email: "ciccio@gmnail.com",
+                date: "12:20"
+              }
+            },
+            {
+              name: 'Apartment room',
+              message: {
+                content: "sdf sdfg sdfg sdf sdf s drh sdfh  sdfh sdf sdf hgsdfh",
+                email: "ciccio@gmnail.com",
+                date: "12:20"
+              }
+            },
+            {
+              name: 'Villa room',
+              message: {
+                content: "sdf sdfg sdfg sdf sdf s drh sdfh  sdfh sdf sdf hgsdfh",
+                email: "ciccio@gmnail.com",
+                date: "12:20"
+              }
+            }
+        ]
+      }
+    }
 }
 
 
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../../../../sass/_variables.scss';
+
+  .container{ 
+    display: flex;  
+    background-color: aquamarine;
+
+    // .menu{
+
+    // }
+    // .messages{
+
+    // }
+  }
 
 </style>
