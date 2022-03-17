dac1a874b0687c778c6a33ea69274363b35867b9
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2022-03-17 14:42:48 +0100

    Create - MessageSend Api + form in Apartment

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
- 
+    protected $validationRule = [
+        "name" => "string|max:100",
+        "content" => "required|string",
+        "email" => "required|string|email|max:255",
+    ];
 
      /**
      * Store a newly created resource in storage.
@@ -17,18 +22,21 @@ class MessageController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {   
+        $request->validate($this->validationRule);
         $data = $request->all();
 
         $newMessage = new Message();
-
         $newMessage->name = $data['name'];
         $newMessage->content = $data['content'];
         $newMessage->email = $data['email'];
+
         $newMessage->stay_id = $data['stay_id'];
 
         $newMessage->save();
-
+        return response()->json([
+           "success" => true 
+        ]);
 
     }
 }
--- resources/js/front/components/pages/Apartment.vue
+++ resources/js/front/components/pages/Apartment.vue
@@ -1,12 +1,73 @@
 <template>
-  <h2>Apartment</h2>
+  <div>
+    <h2>Apartment</h2>
+    <form @submit.prevent="sendMessage()">
+
+        <div>
+          <label for="name">Inserisci il nome </label>
+          <input type="text" v-model="message.name" id="name" >
+          <div v-if="errors.name" >
+            <p v-for="(error, i)  in errors.name" :key="i">
+              {{ error }}
+            </p>
+          </div>
+        </div> 
+
+        <div>
+          <label for="email">Inserisci la mail </label>
+          <input type="email" v-model="message.email" id="email" >
+          <div v-if="errors.email" >
+            <p v-for="(error, i)  in errors.email" :key="i">
+              {{ error }}
+            </p>
+          </div>
+        </div> 
+
+         <div>
+          <textarea type="content" v-model="message.content" placeholder="Inserisci qui il messaggio" rows="4" cols="50"></textarea>
+          <div v-if="errors.content" >
+            <p v-for="(error, i)  in errors.content" :key="i">
+              {{ error }}
+            </p>
+          </div>
+        </div> 
+
+        <button type="submit">Invia Messaggio</button>
+
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
     name: 'Apartment',
+    data() {
+      return {
+        stay: [],
+        message: {
+          name: 'pippo',
+          email:'pippo@gmail.com',
+          content:'forza roma',
+          stay_id:this.$route.params.stay
+        },
+        errors: {}
+      }
+    },
     created() {
-        console.log(this.$route);
+      axios.get(`/api/stays/${this.$route.params.stay}`).then( (response) => {
+        
+        this.stay = response.data
+        console.log(response.data)
+      })
+    },
+    methods: {
+      sendMessage() {
+        axios.post('/api/send-message',this.message).then( (response) => {
+          console.log(response.data)
+        }).catch(error => {
+            this.errors = error.response.data.errors;
+          });
+      }
     }
 }
 </script>
