175456674f526311ff70fd5c029b07a6ee6c4f3c
jimmyhu997 <jimmywx@outlook.it>
2022-03-18 18:39:23 +0100

    Fix Street hint string

--- resources/js/front/components/commons/SearchBar.vue
+++ resources/js/front/components/commons/SearchBar.vue
@@ -113,6 +113,9 @@ export default {
         if (object.streetNumber){
           result += `${object.streetNumber}, `
         }
+        if (object.municipality){
+          result += `${object.municipality},`
+        }
         if (object.countrySecondarySubdivision){
           result += `${object.countrySecondarySubdivision}, `
         }
