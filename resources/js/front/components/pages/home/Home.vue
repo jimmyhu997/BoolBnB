86e038aeec98c39362106943babd487bc7599356
Giovanni <gio.a.98@gmail.com>
2022-03-20 13:07:08 +0100

    navigation through featured listings + responsive

--- resources/js/front/components/pages/home/Home.vue
+++ resources/js/front/components/pages/home/Home.vue
@@ -3,8 +3,18 @@
       <Hero :randomApartment="randomApartment"/>
       <!-- Sponsored apartments -->
       <div class="featured container">
-        <h2 class="featured__title">Featured listings</h2>
-        <ul class="featured__list">
+        <div class="featured__heading">
+          <h2 class="featured__title">Featured listings</h2>
+          <div class="nav-buttons">
+            <button class="round-btn" :class="{'disabled' : scrollLimit == 'min'}" @click="scrollLeft()">
+              <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 4px; overflow: visible;"><g fill="none"><path d="m20 28-11.29289322-11.2928932c-.39052429-.3905243-.39052429-1.0236893 0-1.4142136l11.29289322-11.2928932"></path></g></svg>
+            </button>
+            <button class="round-btn" :class="{'disabled' : scrollLimit == 'max'}" @click="scrollRight()">
+              <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 4px; overflow: visible;"><g fill="none"><path d="m12 4 11.2928932 11.2928932c.3905243.3905243.3905243 1.0236893 0 1.4142136l-11.2928932 11.2928932"></path></g></svg>
+            </button>
+          </div>
+        </div>
+        <ul class="featured__list" ref="featuredList">
           <li class="featured__card" v-for="apartment in apartments" :key="apartment.id" @click="showApartment(apartment.slug)">
             <div class="featured__card-preview">
               <img :src="'storage/' + apartment.image_path" :alt="apartment.title" class="img">
@@ -32,7 +42,10 @@ export default {
     data() {
       return {
         apartments: [],
-        randomApartment: ''
+        randomApartment: '',
+        scrollLimit: 'min',
+        cardListWidth: 0,
+        cardScrollWidth: 0
       }
     },
     created() {
@@ -43,21 +56,66 @@ export default {
           this.randomApartment = slugs[ Math.floor( Math.random() * slugs.length ) ]
         })
     },
+    mounted() {
+      this.cardListWidth = this.$refs.featuredList.getBoundingClientRect().width
+      this.cardScrollWidth = this.$refs.featuredList.scrollWidth
+      window.addEventListener('resize', () => {
+        this.cardListWidth = this.$refs.featuredList.getBoundingClientRect().width
+        this.cardScrollWidth = this.$refs.featuredList.scrollWidth
+      })
+      this.$refs.featuredList.addEventListener('scroll', () => {
+        this.cardScrollWidth = this.$refs.featuredList.scrollWidth
+        if (this.$refs.featuredList.scrollLeft + this.cardListWidth >= this.cardScrollWidth) {
+          this.scrollLimit = 'max'
+        } else if (this.$refs.featuredList.scrollLeft <= 0) {
+          this.scrollLimit = 'min'
+        } else {
+          this.scrollLimit = ''
+        }
+      })
+    },
     methods: {
       showApartment(slug) {
         this.$router.push({name: 'apartment', params: {stay: slug}})
-      }
-    }
+      },
+      scrollLeft() {
+        this.$refs.featuredList.scrollLeft -= 300
+      },
+      scrollRight() {
+        this.$refs.featuredList.scrollLeft += 300
+      },
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../../../../sass/_variables.scss';
+@import '../../../../../sass/mixins.scss';
 .featured {
   margin-bottom: 3rem;
+  &__heading {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 3rem 0 1.5rem;
+    @media screen and (min-width: $medium) {
+      margin: 5rem 0 3rem;
+    }
+    .nav-buttons {
+      display: flex;
+      align-items: center;
+      @include round-btn;
+      .round-btn {
+        margin-left: .5rem;
+      }
+    }
+  }
   &__title {
-    font-size: 2.6rem;
+    font-size: 1.5rem;
     font-weight: 500;
-    margin: 5rem 0 3rem;
+    @media screen and (min-width: $medium) {
+      font-size: 2.6rem;
+    }
   }
   &__list {
     list-style: none;
@@ -65,20 +123,25 @@ export default {
     flex-wrap: nowrap;
     width: 100%;
     overflow-x: scroll;
+    scroll-behavior: smooth;
+    scrollbar-width: none;
+    &::-webkit-scrollbar {
+        display: none;
+    }
   }
   &__card {
     cursor: pointer;
     flex-shrink: 0;
-    margin-right: 2rem;
-    width: 18rem;
+    margin-right: 1rem;
+    width: 10rem;
     border-radius: 1rem;
     overflow: hidden;
     $colors: rgb(211, 25, 64), rgb(172, 0, 90), rgb(190, 23, 58), rgb(205, 37, 37);
     $apartments: 50;
     $j: 1;
     @for $i from 1 through $apartments {
-      @if $i < length($colors) + 1 {
-        $j: $i;
+      @if $j < length($colors)  {
+        $j: $j + 1;
       } @else {
         $j: 1
       }
@@ -88,7 +151,7 @@ export default {
     }
     &-preview {
       width: 100%;
-      height: 13rem;
+      height: 7rem;
       .img {
         object-fit: cover;
         object-position: center;
@@ -97,18 +160,34 @@ export default {
       }
     }
     &-content {
-      padding: 1.3rem;
+      padding: 1rem;
       color: white;
       .title {
-        font-size: 1.6rem;
         font-weight: 500;
         margin-bottom: 1rem;
       }
       .description {
-        margin-bottom: 4rem;
+        margin-bottom: 1rem;
         .location {
-          font-size: 1.2rem;
+          font-size: .9rem;
           font-weight: 300;
+        }
+      }
+    }
+    @media screen and (min-width: $medium) {
+      width: 18rem;
+      margin-right: 2rem;
+      &-preview {
+        height: 13rem;
+      }
+      &-content {
+        padding: 1.3rem;
+        .title {
+          font-size: 1.6rem;
+        }
+        .description {
+          font-size: 1.2rem;
+          margin-bottom: 4rem;
         }
       }
     }
