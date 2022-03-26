ce41f230bfe1f5d3e582aff06d64bbf2e1f1f028
Giovanni <gio.a.98@gmail.com>
2022-03-26 19:27:11 +0100

    update visits seeder

--- database/seeds/VisitSeeder.php
+++ database/seeds/VisitSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Visit;
+use App\Stay;
+use Illuminate\Support\Facades\Auth;
 
 class VisitSeeder extends Seeder
 {
@@ -13,10 +15,15 @@ class VisitSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $stayIds = Stay::select('id')->get()->toArray();
+        
         for($i = 0; $i < 4500; $i++){
+
+            $arr_rand = array_rand($stayIds);
+
             $newVisit = new Visit();
             $newVisit->visitor_ip = $faker->ipv4();
-            $newVisit->stay_id = $faker->numberBetween(1, 2);;
+            $newVisit->stay_id = $stayIds[$arr_rand]['id'];
             $newVisit->created_at = $faker->dateTimeBetween('-220 week', '-1 week');
             $newVisit->updated_at = $faker->dateTimeBetween('-220 week', '-1 week');
             $newVisit->save();
